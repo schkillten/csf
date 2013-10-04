fab101c2b8e30ee33948bd11c49b4bc7d04520a1
schkyl10 <schkyl10@evergreen.edu>
2013-10-04 14:05:45 -0700

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -9,11 +9,13 @@ import math
 import sys
 
 #problem 1 of the homework solving the quadractic
-def quad_minus(a,b,c):#this is for the subtraction half of the quadratic general solution
+#this is for the subtraction half of the quadratic general solution
+def quad_minus(a,b,c):
 	root =(-b - math.sqrt((b**2) + (-4*a*c))) / (2*a)
 	return str(root)
 
-def quad_plus(a,b,c):#this is for the addition half of the quadratic general solution
+#this is for the addition half of the quadratic general solution
+def quad_plus(a,b,c):
 	root =(-b + math.sqrt((b**2) + (-4*a*c))) / (2*a)
 	return str(root)
 	
@@ -27,17 +29,19 @@ if (4*a*c) > (b**2):
 	print "This is an imaginary number. Not currently setup for imaginary numbers"
 	sys.exit(0)
 
-root = quad_minus(a,b,c)#solves for the first half of equation
+#solves for the first half of equation
+root = quad_minus(a,b,c)
 print "This is root #1: %s." % (root)
 print "The  \"tpye\" of root #1 is: ", type(root)
-root = quad_plus(a,b,c)#solves for the second half of equation
+#solves for the second half of equation
+root = quad_plus(a,b,c)
 print "This is root #2: %s." % (root)
 print "The  \"tpye\" of root #2 is: ", type(root)
+#problem 2 of homework importing the mod with values a-f
+from hw1 import *
 
-from hw1 import *#problem 2 of homework
-
-
-answer = ((a and b) or (not c) and not (d or e or f)) #problem 3 of homework
+ #problem 3 of homework solving the equation using values from hw1
+answer = ((a and b) or (not c) and not (d or e or f))
 
 print "The answer for part 3: ", answer
 
