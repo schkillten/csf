e7a65fd797801ae96c6408ee6d0b350e012461ce
schkyl10 <schkyl10@evergreen.edu>
2013-10-17 20:23:34 -0700

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,10 +1,15 @@
-series = raw_input("Enter in a word: ")
-n = input("Enter in a number: ")
-
-print series
+series = raw_input("Select if you would like to get the Fibonacci number or a Sum: ")
+n = input("Enter the number in which you would like calculated: ")
 
 
-if series == "fibonacci":
+series = series.lower()
+series = series[0]
+
+
+
+
+
+if series == "f":
 	pp = 0
 	p = 1
 	for i in range(n - 1):
@@ -12,10 +17,12 @@ if series == "fibonacci":
 		pp = p
 		p = total
 	print total
-
-if series == "sum":
-	total= 0
+elif series == "s":
+	total = 0
 	for i in range(n + 1):
+	
 		total = total + (3*i)
-
 	print total
+else:
+
+	print "Invalid string."
