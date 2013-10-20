253feb7471657be4bceb233135bcd7a83cd627d4
schkyl10 <schkyl10@evergreen.edu>
2013-10-20 15:03:25 -0700

    update

--- homework3/hw3_dna_analysis.py
+++ homework3/hw3_dna_analysis.py
@@ -65,7 +65,7 @@ for bp in seq:
     if bp == 'C' or bp == 'G':
         # increment the count of gc
         gc_count = gc_count + 1
-    if bp == "A" or bp == "T":
+    if bp == 'A' or bp == 'T':
 	at_count = at_count + 1
 
 # divide the gc_count by the total_count
